2ea9408e77a76e69f475a3c3df9e2d62f729bfbb
HectorGit <hectorandres.pv@gmail.com>
2022-06-20 09:38:26 -0700

    define additional data models

--- dashboardproject/core/models.py
+++ dashboardproject/core/models.py
@@ -8,3 +8,22 @@ class Notes(models.Model):
 
     def __str__(self):
         return self.title
+
+# Possible models we will ---NEED--- to define : 
+
+# bike_user_data 
+# automatic column (id)
+# user_id int4 
+# hour int4
+# date date 
+# time_bike int4
+# distance float4
+# calories float4
+# power float4
+# timestamp timestamp
+
+# Primary Key : user_id + hour + date
+
+#------------------------------------------------------
+
+# Models that would be ---NICE TO HAVE--- to define :
